5f7bfcc6fba0c1e8a77159049e20a0c08173dde5
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-05-07 13:11:14 -0500

    agregar el auth.guard.ts

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+export class AuthGuard implements CanActivate{
+  
+  constructor( private router: Router, private authService: AuthService) {}
+
+  // canActivate(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canActivateChild(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      const currentUser = this.authService.isLoggedIn;
+      if (currentUser) {
+          // autorizado así que devuelve verdadero
+          return true;
+      }
+      //no ha iniciado sesión, así que redirige a la página de inicio de sesión con la URL de retorno
+      this.router.navigate(['/landing']);
+      return false;
   }
 }
